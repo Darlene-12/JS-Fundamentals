3605ab11e2ef93f5f758c2bfc0d59406e59799f6
Darlene-12 <darlenewendie@gmail.com>
2025-07-09 11:15:23 +0300

    Corrected 3-value...

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,8 +1,8 @@
-const args = process.argv.slice(2);
+const arg = process.argv[2];
 
-if (args.length === undefined){
+if (arg === undefined){
     console.log("No argument");
 }
 else {
-    console.log(args[0])
+    console.log(arg);
 }
